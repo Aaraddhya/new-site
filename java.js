de5ab028f306241e99287eafc7eb4d18c706d356
aaraddhya <iamaaraddhya@gmail.com>
2017-05-27 23:40:26 +0530

    Frame_done_1

---
+++ java.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  /*var obj = $("#container"),
+        w = obj.attr('aria-width'),
+        h = obj.attr('aria-height');
+  setTimeout(function(){
+    obj.css('width',w);
+  },1000);
+  setTimeout(function(){
+    obj.css('height',h);
+  },1600);*/
+});
